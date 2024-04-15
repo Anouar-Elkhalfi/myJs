d3ae06f8a6e31c0f9bde4937fb163da074fa588f
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-15 16:51:09 +0200

    var declaration

---
+++ index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count").innerText = 5
+// let count = 5+7;
+// // console.log(count);
+// let firstbatch = 5 ;
+// console.log(firstbatch);
+// let secondbatch = 9;
+// console.log(secondbatch);
+// let count = firstbatch + secondbatch
+// console.log(count);
+// // let myAge = 35;
+// console.log(myAge);
+// let myAge = 35;
+// let humanDogRate = 7;
+// let myDogAge = myAge * humanDogRate;
+// console.log(myDogAge);
+ let count = 5;
+ count = count + 1;
+ console.log(count);
+
+ 
