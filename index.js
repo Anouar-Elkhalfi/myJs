e4a5ba00b7d7d1748b365e8a6db071b50421d21e
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-16 11:18:10 +0200

    exemple de déclaration de variables

--- index.js
+++ index.js
@@ -13,8 +13,17 @@
 // let humanDogRate = 7;
 // let myDogAge = myAge * humanDogRate;
 // console.log(myDogAge);
- let count = 5;
- count = count + 1;
- console.log(count);
+//  let count = 5;
+//  count = count + 1;
+//  console.log(count);
 
- 
+ // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 100;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 25;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 70;
+// console.log(bonusPoints);
